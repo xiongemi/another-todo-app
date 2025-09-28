02031e51982bd8928819441e6c9da550e15d424f
Emily Xiong <xiongemi@gmail.com>
2025-09-28 14:59:58 -0400

    Initial commit

---
+++ apps/another-todo-app/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  displayName: '@another-todo-app/another-todo-app',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/another-todo-app',
+  testEnvironment: 'jsdom',
+};
+
+export default createJestConfig(config);
