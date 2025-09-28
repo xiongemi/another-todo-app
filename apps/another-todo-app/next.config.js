3940e56f8540245e38698617e722de84b2569a1f
Emily Xiong <xiongemi@gmail.com>
2025-09-28 17:54:12 -0400

    add deploy flow

--- apps/another-todo-app/next.config.js
+++ apps/another-todo-app/next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
+  output: 'export',
+  basePath: '/another-todo-app',
+  assetPrefix: '/another-todo-app',
+  distDir: '/another-todo-app'
 };
 
 const plugins = [
