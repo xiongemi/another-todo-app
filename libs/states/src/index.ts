29b748139ffd1ddbafef06e623c15f8a170d7073
Emily Xiong <xiongemi@gmail.com>
2025-11-09 16:27:41 -0500

    fix build

--- libs/states/src/index.ts
+++ libs/states/src/index.ts
@@ -1,3 +1,15 @@
-export * from './lib/store';
-export * from './lib/todosSlice';
-export * from './lib/hooks';
+// Explicit re-exports to ensure TypeScript picks up values in project refs
+export { createAppStore, store, persistor } from './lib/store';
+export type { AppDispatch } from './lib/store';
+
+export {
+  todosSlice,
+  addTodo,
+  toggleTodo,
+  setDayTodos,
+  selectAllByDay,
+  selectDayTodos,
+} from './lib/todosSlice';
+export type { RootState } from './lib/todosSlice';
+
+export { useAppDispatch, useAppSelector } from './lib/hooks';
